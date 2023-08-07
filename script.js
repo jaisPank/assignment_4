d781881cb2e5bf58248c620018960d421f0f1bc1
jaisPank <pankaj14jaiswal@gmail.com>
2023-08-07 12:50:00 +0530

    Update with Images

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const apiKey = '994b11f5e1be4f01106f3a62d2bd6f48'; // Replace with your API key 
 const searchBtn = document.getElementById('searchBtn');
 const locationInput = document.getElementById('locationInput');
 const weatherInfoDiv = document.getElementById('weatherInfo');
+const weatherIcon = document.querySelector('.weather-icon');
+
 
 searchBtn.addEventListener('click', () => {
     const location = locationInput.value.trim();
@@ -28,19 +30,57 @@ async function getWeatherData(location) {
 function displayWeatherData(data) {
     const location = `${data.name}, ${data.sys.country}`;
     const temperature = `${data.main.temp} °C`;
-    const description = data.weather[0].description;
+    const description = data.weather[0].main;
     const humidity = `Humidity: ${data.main.humidity}%`;
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = `${Math.round(data.main.temp)} °C`;
+    document.querySelector('.weatherCondition').innerHTML = data.weather[0].main;
+    document.querySelector('.humidity').innerHTML = `${data.main.humidity}%`;
+    document.querySelector('.wind').innerHTML = `${data.wind.speed} km/h`;
 
-    const weatherInfoHTML = `
-        <h2>${location}</h2>
-        <p>${temperature}</p>
-        <p>${description}</p>
-        <p>${humidity}</p>
-    `;
 
-    weatherInfoDiv.innerHTML = weatherInfoHTML;
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+    else if(data.weather[0].main == "Haze"){
+        weatherIcon.src = "images/haze.png";
+    }
+
 }
 
 function displayError(errorMessage) {
     weatherInfoDiv.innerHTML = `<p class="error">${errorMessage}</p>`;
+}
+
+
+
+
+const unitToggle = document.getElementById('unitToggle');
+const unitLabel = document.getElementById('unitLabel');
+
+let isCelsius = true; // Default to Celsius
+
+unitToggle.addEventListener('change', () => {
+    isCelsius = !isCelsius;
+    updateTemperature();
+});
+
+function updateTemperature() {
+    const temperature = parseFloat(document.querySelector('.temp').textContent);
+    const updatedTemperature = isCelsius ? temperature : (temperature * 9/5) + 32;
+    const unitText = isCelsius ? 'Celsius' : 'Fahrenheit';
+    unitLabel.textContent = unitText;
+    document.querySelector('.temp').textContent = updatedTemperature.toFixed(2);
 }
